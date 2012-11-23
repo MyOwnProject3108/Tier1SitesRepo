ad03f42a5b5e2b56b505263db8907c4506c4fe02
Peter Hartman <me@peterhartman.co.uk>
2012-11-23 22:27:04 +0000

    Added tracker plugin

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,9 +1,19 @@
 require 'watir-webdriver'
 
+profile = Selenium::WebDriver::Firefox::Profile.new
+profile['extensions.tracker.url'] = "//test1.lan/tracker/peerius.page"
+profile['extensions.tracker.enabled'] = false 
+profile['extensions.tracker.sites'] = "demo.peerius.com.NEXT.ctshirts.co.uk.NEXT.lovehoney.co.uk"
+profile.add_extension "features/support/peerius-tfp@peerius.co.uk.xpi"
+browser = Watir::Browser.new :firefox, :profile => profile
+
 Before('~@smartapi') do
-    @browser = Watir::Browser.new :firefox          
+   @browser = browser
 end
 
 After('~@smartapi') do 
-    @browser.close
+end
+
+at_exit do
+  browser.close
 end
