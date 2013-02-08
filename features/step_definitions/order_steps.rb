b396f54993a8e25ff2dcfe45bbebe36e110e6d8b
faiyyazsultana <faiyyaz.sultana@peerius.com>
2013-02-08 13:29:23 +0000

    Fixed Topman tests

--- features/step_definitions/order_steps.rb
+++ features/step_definitions/order_steps.rb
@@ -1,9 +1,6 @@
-When /^I purchase "(.*?)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
 
 When /^I add the current product to the basket$/ do
-  @current_page.add_to_basket_element.click
+  @current_page.add_to_basket_element.when_visible.click
 end
 
 When /^I click checkout$/ do
