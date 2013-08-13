06b4f3a084d9d7c3772c357dd16e6ddd142de73e
faiyyazsultana <faiyyaz.sultana@peerius.com>
2013-08-13 10:14:00 +0100

    tier1

--- features/SMART_manager_feature_files/SM_email_setup_05.feature
+++ features/SMART_manager_feature_files/SM_email_setup_05.feature
@@ -15,32 +15,6 @@ Given I am on the smartmanager login page
  Then I should end up on the email page
  When I click on the create link on the email page
  Then I should end up on the emailcreate page
-  
-Scenario: Users should be able to create email widget
-  And I enter email name as "AutoTestEmail"
-  And I enter email content width as 200 and height as 140
-  And  I select title color "blue"
-  And I select price color "red"
-  And I select background color "yellow"
-  And I pause for 5 seconds
- # And I select no repeat recs as 1 on the emailcreate page
-  And I pause for 10 seconds
-  When I drag and drop "Abandoned Basket" rule to the position slot on the emailcreate page
-  And I pause for 5 seconds
- And copy position 1 configuration to position 2
-  And I enter email as "webtestpeerius@mailinator.com"
-  And I click on the trigger test email link
-  And I pause for 10 seconds
-  And I go to the mailinator page
-  And I enter email as "webtestpeerius@mailinator.com" on the mailinator page
-  When I click on the Go button on the mailinator page
-  And I pause for 5 seconds
-  Then I should see Our Recommendations link on the mailinator page
-  #When I open the first Our Recommendations email on the mailinatorinbox page
-  #And I pause for 5 seconds
-# Then I should see peerius recommendations in the email body
- # And I pause for 5 seconds
-  #Then I should see "This is the sample email with Peerius Recommendations." in the email body
 
 Scenario: Verifying the strategy "New Products(last 14 days)" in the debug
   And I enter email name as "AutoTestEmail"
@@ -278,7 +252,37 @@ Scenario: Verifying the strategy "New products from favourite categories(last 12
   And I pause for 10 seconds
   Then I should see "New products from favourite categories(last 120 days)" on the emaildebug page
 
-  
+   
+Scenario: Verifying the email triggering functionality with no repeat recs
+  And I enter email name as "AutoTestEmail43"
+  And I enter email content width as 200 and height as 140
+  And  I select title color "blue"
+  And I select price color "red"
+  And I select background color "yellow"
+  And I pause for 5 seconds
+  And I select no repeat recs as 1 on the emailcreate page
+  And I pause for 10 seconds
+  When I drag and drop "New products from favourite categories(last 120 days)" rule to the position slot on the emailcreate page
+  And I pause for 5 seconds
+ And copy position 1 configuration to position 2
+#  And I enter email as "webtestpeerius@mailinator.com"
+ # And I click on the trigger test email link
+#  And I pause for 10 seconds
+ # And I go to the mailinator page
+  And I enter email as "webtestpeerius@mailinator.com"
+  And I click on the preview email link on the emailcreate page
+  And I pause for 10 seconds
+  When I click on the debug information link on the emailcreate page
+  And I pause for 10 seconds
+  Then I should see either "(Best Sellers by Revenue ( last 28 days)|Product Catalog)" on the emaildebug page
+#  When I click on the Go button on the mailinator page
+ # And I pause for 5 seconds
+#  Then I should see Our Recommendations link on the mailinator page
+  #When I open the first Our Recommendations email on the mailinatorinbox page
+  #And I pause for 5 seconds
+# Then I should see peerius recommendations in the email body
+ # And I pause for 5 seconds
+  #Then I should see "This is the sample email with Peerius Recommendations." in the email body 
   
   
   
--- features/step_definitions/smartmanager_email_steps.rb
+++ features/step_definitions/smartmanager_email_steps.rb
@@ -56,108 +56,6 @@ Then /^I select background color "(.+)"$/ do |color|
   @current_page.div_element(:id=>"color_selector").div_element(:class =>"color_swatch", :index => index).click
 end
 
-#When /^I drag and drop "(.+)" rule to the position (\d+) slot$/ do |algorithm, position|
-#  index = position.to_i - 1
- # rule = @browser.span(:text => algorithm)
-  #slot = @browser.div(:id => index.to_s)
-  
-#@browser.span(:class => "draggable ui-draggable").drag_and_drop_on(@browser.div(:class => "droparea ui-droppable ui-sortable"))
-  
- # @browser.driver.action.drag_and_drop(rule.wd, slot.wd).perform
- # @browser.driver.action.drag_and_drop(rule, slot).perform
- # rule.fire_event("onmousedown")
-  #driver = @browser.driver
-  #driver.action.click_and_hold(rule.wd).perform
-
-  #sleep 2
-  #driver.action.move_to(slot.wd).perform
-
-  #sleep 2
-  #rule.fire_event("onmouseup")
-####  @browser.span(:text=>"algorithm").wd.drag_and_drop_on(@browser.div(:id=>"0").wd)
-
-
-  #rule.drag_and_drop_on(slot)
-  
-#@browser.span(:text => algorithm).drag_and_drop_by 100, -200
- #   def drag_and_drop_by(right_by, down_by)
-  #    assert_exists
-   #   assert_has_input_devices_for :drag_and_drop_by
-#
- #     @browser.action.
-  #           drag_and_drop_by(@element, right_by, down_by).
-   #          perform
-    #end
-#end
-
-#When(/^I drag and drop Abandoned Basket rule to the position slot on the emailcreate page$/) do
-#  @browser.span(:class => "draggable ui-draggable").drag_and_drop_on(@browser.div(:class => "droparea ui-droppable ui-sortable"))
-  #button_1 = @browser.span(:class => "draggable ui-draggable")
-  #button_2 = @browser.div(:class => "droparea ui-droppable ui-sortable")
- # target = @browser.div(:id=>"0")
-
-  #my_element.fire_event("onmousedown")
-
-  #driver=browser.driver
-  #@browser.click_and_hold(my_element.wd).perform
-
-  #sleep 2
-  #@browser.move_to(target.wd).perform
-
-  #sleep 2
-  #target.fire_event("onmouseup")
-  #@browser.span(:class => "draggable ui-draggable").wd.drag_and_drop_on(@browser.div(:class => "droparea ui-droppable ui-sortable").wd)
-#  @browser.driver.action.click_and_hold(button_1.wd).move_to(button_2.wd).release.perform
-  #action = @browser.driver.action
-#  action.key_down(:control)
- #a = @browser.span(:class => "draggable ui-draggable")
- #b = @browser.div(:class => "droparea ui-droppable ui-sortable")
-#pp @browser.find_element(:class => "draggable ui-draggable")
-#  a = @browser.span(:class => "draggable ui-draggable").wd
- #b = @browser.div(:class => "droparea ui-droppable ui-sortable").wd
- #####working#####
-# a = @browser.span(:text => "Abandoned Basket")
- #sleep 10
- #b = @browser.div(:class => "droparea ui-droppable ui-sortable")
- #sleep 10
-#@browser.driver.action.drag_and_drop(a,b).perform
-#a.drag_and_drop_on b
-#	def drag_and_drop_on(other)
- #     assert_exists
-  #    driver.action.drag_and_drop(@element, other.wd).perform
-   # end
-   
-   ########Its working###########
- #  When(/^I drag and drop "(.*?)" rule to the position slot on the emailcreate page$/) do |algorithm|
-#	algorithm = ['Abandoned Basket','Product Catalog','Wishlist highly viewed']
-	#email_strategy.each do |y|
-#	my_element = @browser.span(:text => 'email_strategy')
-##working####
-#my_element = @browser.span(:text => "Abandoned Basket")
-#my_element = @browser.span(:text => algorithm)
-
-#working### 
-	#target = @browser.div(:class => "droparea ui-droppable ui-sortable")
-#target = @browser.div(:id => "0")
-#working####
-	#my_element.fire_event("onmousedown")
-	#@browser.driver.action.click_and_hold(my_element.wd).perform
-	#sleep 10
-#@browser.driver.action.move_to(target.wd).move_by(10, 10).click.perform
-##working####
-	#@browser.driver.action.move_to(target.wd).click.perform
-	#sleep 5
-##@browser.driver.action.release.perform
-	#if y=="Abandoned Basket"
-	#working###
-	#	my_element.fire_event("onmouseup")
-	#end
-	# @browser.driver.action.key_up(:control)
- #action.perform
-# a.drag_and_drop_on(b)
- #@browser.action.drag_and_drop(a, b).perform
- #end
- #end
 
  
  When(/^I drag and drop "(.*?)" rule to the position slot on the emailcreate page$/) do |algo|
@@ -219,32 +117,22 @@ puts (@browser.text.include? expected_result).should == true
  
 	@browser.window(:title => "Peerius").close
 end
- #if @browser.text.should == expectedstrategy
-#puts "Test passed!"
-#else
-#puts "Test failed!"
-#end
-#puts on_page(Smartmanager::EmaildebugPage).text.include? "expectedstrategy"
- #on_page(Smartmanager::EmaildebugPage).expected_strategy_element.exists?
-#pp on_page(Smartmanager::EmaildebugPage).expectedstrategy.eql?(expectedstrategy).should == true 
-#on_page(Smartmanager::EmaildebugPage).expectedstrategy1.eql?(expectedstrategy1).should == true 
-#	x = @expectedstrategy
-	#if @expectedstrategy['color'] == "red"
-	#@expectedstrategy
-#	if(x == expectedstrategy)
-#	puts expectedstrategy
-#	else if(x == 'Product Catalog')
-#	puts expectedstrategy
-#	if
-#	puts false
-#	if(x='Product Catalog' || 'Best Sellers by Conversion (last 90 days)')
-#	puts true
-#	else
-#	puts false
-#	end
-#end
-#end
 end
+
+
+Then(/^I should see either "(.*?)" on the emaildebug page$/) do |algorithm|
+  @browser.window(:title => "Peerius").use do
+#  fail unless 
+#puts (@browser.text.include? ('Best Sellers by Revenue ( last 28 days)'|| 'Product Catalog')).should == true
+#puts (@browser.text.include? ('Best Sellers by Revenue ( last 28 days)'|| @browser.text.include? 'Product Catalog')).should == true
+puts @browser.text.should =~ /(Best Sellers by Revenue ( last 28 days)|Product Catalog)/
+ # puts @browser.text.should == 'Best Sellers by Revenue ( last 28 days)' || @browser.text.should == 'Product Catalog'
+   pp @browser.elements(:text => algorithm).size
+ pp @browser.td(:text => algorithm)
+ @browser.window(:title => "Peerius").close
+end
+end
+
 
 When(/^I enter email as "(.+)" on the mailinator page$/) do |emailrecs|
   on_page(Smartmanager::MailinatorPage).emailrecs = emailrecs
