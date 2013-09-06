fcec0375d5e9620b83c5eb91866549e04974ee93
faiyyazsultana <faiyyaz.sultana@peerius.com>
2013-09-06 10:00:37 +0100

    SMART mail

--- features/step_definitions/smartmanager_email_steps.rb
+++ features/step_definitions/smartmanager_email_steps.rb
@@ -56,6 +56,18 @@ Then /^I select background color "(.+)"$/ do |color|
   @current_page.div_element(:id=>"color_selector").div_element(:class =>"color_swatch", :index => index).click
 end
 
+When(/^I uncheck the top\-ups checkbox$/) do
+ @current_page.topups_submit_element.click
+end
+
+Then(/^I should read the algorithm,hint and expression on the emaildebug page$/) do
+  @browser.window(:title => "Peerius").use do
+ pp @browser.table.tr(:index => 8).text
+ pp @browser.table.tr(:index => 9).text
+  pp @browser.table.tr(:index => 11).text
+  @browser.window(:title => "Peerius").close
+  end
+end
 
  
  When(/^I drag and drop "(.*?)" rule to the position slot on the emailcreate page$/) do |algo|
@@ -90,7 +102,7 @@ When /^I enter email as "(.+)"$/ do |email|
   @current_page.email = email
 end
 
-When /^I click on the trigger test email link$/ do
+When /^I click on the trigger test email link on the emailcreate page$/ do
   @current_page.test_email_link_element.click
 end
 
@@ -103,52 +115,91 @@ When(/^I click on the debug information link on the emailcreate page$/) do
 end
 
 Then(/^I should see "(.*?)" on the emaildebug page$/) do |expected_result|
-#puts on_page(Smartmanager::EmaildebugPage).text.include?("expectedstrategy").should == false
-#puts @browser.text.include? '$expectedstrategy'
-	@browser.window(:title => "Peerius").use do
-	#@browser.text.should == expected_result
-#	@browser.table(:xpath => "//*[@id='peeriusDebug']/tbody/tr[9]/td").text
-#result= @browser.text.include? expected_result
-#puts result.size
-puts (@browser.text.include? expected_result).should == true
- #   @browser.text.should == expected_result
- pp @browser.elements(:text => expected_result).size
- pp @browser.td(:text => expected_result)
+  @browser.window(:title => "Peerius").use do
+  puts (@browser.text.include? expected_result).should == true
+ # working pp @browser.elements(:text => expected_result).size
+ pp @browser.table.tr(:index => 8).text
+ pp @browser.table.tr(:index => 9).text
+# working pp @browser.td(:text => expected_result)
  
 	@browser.window(:title => "Peerius").close
 end
 end
 
+Then(/^I should see "(.*?)" message on the emailcreate page$/) do |emailtrigger|
+  @current_page.text.include? 'emailtrigger'
+end
+
 
 Then(/^I should see either "(.*?)" on the emaildebug page$/) do |algorithm|
   @browser.window(:title => "Peerius").use do
-#  fail unless 
-#puts (@browser.text.include? ('Best Sellers by Revenue ( last 28 days)'|| 'Product Catalog')).should == true
-#puts (@browser.text.include? ('Best Sellers by Revenue ( last 28 days)'|| @browser.text.include? 'Product Catalog')).should == true
-#puts @browser.text.should =~ /(Best Sellers by Revenue ( last 28 days)|Product Catalog)/
-#puts (@browser.window(title: 'Peerius').@browser.text.include? algorithm).should == true
-#puts (@browser.text.include? algorithm).should == true
-
-##below code working###
-#puts @browser.text.include?("New Products(last 14 days)") || @browser.text.include?("Product Catalog")
-
-if @browser.text.include?("New Products(last 14 days)") || @browser.text.include?("Product Catalog")
-puts "test passed..."
-else
-puts "test failed"
+   puts @browser.text.include?("NewProduct") || @browser.text.include?("Catalog")
+ pp @browser.table.tr(:index => 8).text
+ pp @browser.table.tr(:index => 9).text
+end
+end
 
 
-#puts (@browser.text.include? (algorithm) || @browser.text.include? (algorithm)).should == true
-#puts (@browser.text.include? algorithm).should == true || (@browser.text.include? algorithm).should == true
+$rs1 = Array.new
+$rs2 = Array.new
+#$rs1.push("test")
+#puts "rs1 before ="+ $rs1
 
- # puts @browser.text.should == 'Best Sellers by Revenue ( last 28 days)' || @browser.text.should == 'Product Catalog'
- #puts @browser.attach(:title => "Peerius").text
-  # pp @browser.window(:title => "Peerius").elements(:text => algorithm).size
- #puts @browser.window(:title => "Peerius").td_element(:text => algorithm)
+Then(/^I read and compare the first two refcodes in the emaildebug page$/) do
+  @browser.window(:title => "Peerius").use do
+ str1 = @browser.table.th(:index => 2).text ###working
+ str2 = @browser.table.th(:index => 11).text
+ $rs1.push(str1.split(":")[1])
+ $rs2.push(str2.split(":")[1])#[1]
+ puts "rs1 ="+ $rs1.to_s
+ puts "rs2="+ $rs2.to_s
+ puts $rs1[0].eql?($rs1[1])
  @browser.window(:title => "Peerius").close
+ end
 end
+
+When(/^I select the hint as "(.*?)" on the emailcreate page$/) do |hint|
+  @current_page.hint = hint
 end
+
+When(/^I click on the expression on the emailcreate page$/) do
+  @current_page.expression_submit_element.click
 end
+
+When(/^I click on the add expression link on the emailcreate page$/) do
+  @current_page.add_expression_link_element.click
+end
+
+When(/^I click on the "(.*?)" link on the emailcreate page$/) do |attribute|
+  @current_page.attribute_submit_element.click
+end
+
+When(/^I hover over on the Attributes link on the emailcreate page$/) do
+  @current_page.link_element(:href => '#attrs').hover
+end
+
+Then(/^I should see "(.*?)" error message on the emailcreate page$/) do |emailerrormessage|
+@current_page.text.include? 'emailerrormessage'
+end
+
+Then(/^I should read the algorithm in the emaildebug page$/) do
+  @browser.window(:title => "Peerius").use do
+  pp @browser.table.tr(:index => 8).text
+ pp @browser.table.tr(:index => 9).text
+  @browser.window(:title => "Peerius").close
+ end
+end
+
+
+When(/^I select the Attributes as "(.*?)" on the emailcreate page$/) do |attrselect|
+  @current_page.attrselect_link_element.click
+end
+
+
+When(/^I enter attribute as "(.*?)" on the emailcreate page$/) do |attr|
+ @current_page.attr = attr
+end
+
 
 When(/^I enter email as "(.+)" on the mailinator page$/) do |emailrecs|
   on_page(Smartmanager::MailinatorPage).emailrecs = emailrecs
@@ -167,11 +218,12 @@ When /^I click on the save button on the emailcreate page$/ do
   @current_page.save_email_submit_element.click
 end
 
-When /^I click on the delete link on the email page$/ do
-  @current_page.delete_email_link_element.click
+When /^I click on the first delete link on the email page$/ do
+  #(@current_page.delete_email_link_element).first.click
+  @current_page.image_elements(:src => "/tracker/css/images/delete.png").first.click
 end
 
-When /^I confirm the delete email widget$/ do
+When /^I confirm the delete email widget on the email page$/ do
   @current_page.confirm_delete_email_element.click
 end
 
