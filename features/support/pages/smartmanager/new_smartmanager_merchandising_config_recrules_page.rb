f5724c476c3c8f4fd8ec9bfe2fe919d34e130fa8
unknown <ajoshi@EMBIOPTERA.peerius.local>
2013-10-08 11:35:30 +0100

    merchandising

---
+++ features/support/pages/smartmanager/new_smartmanager_merchandising_config_recrules_page.rb
@@ -0,0 +1,32 @@
+module Smartmanager
+  
+    class RecrulesconfigPage
+            
+      include PageObject
+      include PeeriusHelper
+      
+      URL = "https://#{FigNewton.base_url}/tracker/smartmanager/merchandising/edit.page"
+      direct_url URL
+	  h4(:page_identifier, :text => "Recommendation Rules")
+	  
+	  #select_list(:setexpression_submit, :class => "exp_left_hand")
+	 # select_list(:set_expression, :text => "Please Select")
+	  
+	  button(:edit_rule_submit, :class => "btn btn-small pull-left edit-rule-settings-btn")
+	  
+	 # text_field(:numwidgets, :id => "howMany")
+	 # button(:create_email1_submit, :id => "mail_submit_btn")
+	 # text_field(:strategy_select, :class => "visual-input")
+	#  li(:emailstrategy, :class => "visual-item BESTSELLER_BY_REVENUE_SMART_CATEGORY")
+	#  li(:emailstrategy, :class => "visual-item BESTSELLER_BY_REVENUE_SMART_CATEGORY")
+  
+	
+	 	 
+	 def tracked_as
+	   return "RecrulesconfigPage"
+	  end
+				  
+    end  
+end
+
+
