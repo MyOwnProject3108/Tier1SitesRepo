d86db48a567ad6dfddffcbf7eedef54f357d3616
faiyyazsultana <faiyyaz.sultana@peerius.com>
2014-01-07 12:05:16 +0000

    tier1 and smart content

--- features/support/pages/smartmanager/new_smartmanager_smartcontent_adaptivecontent_page.rb
+++ features/support/pages/smartmanager/new_smartmanager_smartcontent_adaptivecontent_page.rb
@@ -9,6 +9,11 @@ module Smartmanager
 		h4(:page_identifier, :text => "Setup")
 		text_field(:testsetup, :id => "name")
 		a(:criterialink_submit, :href => "#criteriaoptions")
+		#text_field(:rule1, :class => "exp_right_hand the_value_input input-small autosearch-original original disabled")
+		text_field(:rule1, :xpath => "//div[@id='traffic']/table/tbody/tr/td[3]/div/div[3]/ul/li/ul/li/div/div[1]/input")
+		button(:setup_submit, :class => "btn remove-btn save_changes btn-primary pull-right")
+		span(:add_a_new_row, :class => "text-success unselectable", :index => 1)
+		#div(:rule1, :class => "autosearch-wrap autosearch exp_right_hand_autocomplete clear clearfix")
 				
 
 	 	 
