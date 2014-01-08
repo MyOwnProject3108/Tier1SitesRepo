188fa5344c560ea49e4b74ab1c022b79033bc6d9
Manoj Gopinath <manoj.gopinath@peerius.com>
2014-01-08 15:31:50 +0000

    Refactored for cleaner logging.

--- utils/qa_generate/qa_generate.rb
+++ utils/qa_generate/qa_generate.rb
@@ -52,6 +52,22 @@ site_files = Dir[input_file]
 sitelist_file_mode = "w"
 dbmapping_file_mode = "w"
 
+cur_path = File.dirname(__FILE__)
+sites_yaml_path = cur_path + "/sites"
+site_feature_path = File.expand_path("../..",Dir.pwd)
+utils_path = File.expand_path("..",Dir.pwd) 
+
+puts "CONFIG => "
+puts "\tFeature templates are in #{cur_path}/templates/features/"
+puts "\tSmart Product step definition templates are in #{site_feature_path}/features/step_definitions"
+puts "\tTracking Step definition templates for each page type are in #{cur_path}/templates/features/support/pages/site"
+puts "\tSite configuration (<sitename>.yaml) files are in #{sites_yaml_path}/"
+
+puts "OUTPUT => "
+puts "\tFeature files for each site will be generated in #{utils_path}/features/"
+puts "\tPage step definition code will be generated in #{utils_path}/features/support/pages/<sitename>/"
+
+puts "GENERATE => "
 site_files.each do |site_filename|
   # Load the site YAML file
   #site = YAML::load(File.open(site_filename, 'r:utf-8'))
@@ -62,7 +78,20 @@ site_files.each do |site_filename|
   output_path = "../../" unless opts[:outfile]
   output_path << '/' if output_path[-1].chr != '/' 
   
+  # Generate sitelist
+  autosites_filename = output_path + "features/support/auto_sites.txt"
+  puts "Creating #{autosites_filename}..." unless sitelist_file_mode == "a" 
+  FileUtils.mkpath File.dirname(autosites_filename)
+  
+  # Generate dbmapping
+  dbmapping_filename = output_path + "features/support/auto_dbmapping.yaml"
+  puts "Creating #{dbmapping_filename}..." unless dbmapping_file_mode == "a" 
+  FileUtils.mkpath File.dirname(dbmapping_filename)
+   
   # Generate output files from templates
+  # puts "#{site_filename.partition('/').last.upcase}: Generating features from #{sites_yaml_path + site_filename.partition('/').last} \n \t\t\t using templates in #{cur_path}templates/features"
+  puts "\t#{site_filename.partition('/').last.partition('.').first.upcase} \t :: Generating feature files, scenarios and step definition code"
+
   template_files.each do |template_filename|
     if File.file?(template_filename)
       template_file = File.open(template_filename, "r") { |f| f.read }
@@ -70,44 +99,37 @@ site_files.each do |site_filename|
       generator.filename = template_filename
       output_content = generator.result(binding)
       
-      output_filename = output_path + template_filename.gsub(/#{template_directory}/,'').gsub(/site/, site["site_name"])
+      output_filename = site_feature_path + "/" + template_filename.gsub(/#{template_directory}/,'').gsub(/site/, site["site_name"])
       if (output_content =~ /\A#ignore/) then
-        puts "Skipping #{output_filename}. No tests to run." 
+        # puts "Skipping #{output_filename}. No tests to run." 
       else
-        puts "Creating #{output_filename}..."
+        # puts "Creating #{output_filename}..."
         FileUtils.mkpath File.dirname(output_filename)
         output_file = File.open(output_filename, "w")
         output_file << output_content
       end
-      
+       # puts output_filename
     end    
   end
   
-  # Generate sitelist
-  output_filename = output_path + "features/support/auto_sites.txt"
-  puts "Creating #{output_filename}..." unless sitelist_file_mode == "a" 
-  FileUtils.mkpath File.dirname(output_filename)
-   
-  File.open(output_filename, sitelist_file_mode) do |file|
+  File.open(autosites_filename, sitelist_file_mode) do |asfile|
     site_name = site["home_page"]["URL"]
     site_name.gsub!(/https?:\/\//,'')
     site_name.gsub!(/www\./,'')
     site_name.gsub!(/\/.*$/, '')
-    file.puts site["home_page"]["URL"] + "\n"
+    asfile.puts site["home_page"]["URL"] + "\n"
   end
   
   sitelist_file_mode = "a"
-  
-  # Generate dbmapping
-  output_filename = output_path + "features/support/auto_dbmapping.yaml"
-  puts "Creating #{output_filename}..." unless dbmapping_file_mode == "a" 
-  FileUtils.mkpath File.dirname(output_filename)
-   
-  File.open(output_filename, dbmapping_file_mode) do |file|
-    file.puts "#{site["site_name"]}:\n"
-    file.puts "  db: #{site["database_num"]}" unless site["database_num"].nil?
-    file.puts "  site_id: #{site["site_id"]}" unless site["site_id"].nil?
+  File.open(dbmapping_filename, dbmapping_file_mode) do |dbmfile|
+     dbmfile.puts "#{site["site_name"]}:\n"
+     dbmfile.puts "  db: #{site["database_num"]}" unless site["database_num"].nil?
+     dbmfile.puts "  site_id: #{site["site_id"]}" unless site["site_id"].nil?
   end
+      
+  dbmapping_file_mode = "a"
   
-  dbmapping_file_mode = "a"   
+
 end
+
+puts "========= ALL DONE ========"
