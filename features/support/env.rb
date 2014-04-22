e39635186f135bd258ab1c33f9cc2a4e8e8bf2a2
root <root@showcasebe.(none)>
2014-04-22 20:33:41 +0100

    Added config to allow cucumber to run in headless mode

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,3 +4,14 @@ require 'page-object/page_factory'
 require 'fig_newton'
 require_relative 'peerius/peerius_helper'
 World(PageObject::PageFactory)
+
+if ENV['HEADLESS'] == 'true'
+  require 'headless'
+
+  headless = Headless.new
+  headless.start
+
+  at_exit do
+    headless.destroy
+  end
+end
