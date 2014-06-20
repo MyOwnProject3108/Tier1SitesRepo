dafff4fb42b9b84151e83c61349c6cfe0235008f
tmckiernan <thomas.mckiernan@peerius.com>
2014-06-20 16:37:51 +0100

    optional text fields for lovehoney dual basket->checkout process

--- features/step_definitions/raw_webdriver_steps.rb
+++ features/step_definitions/raw_webdriver_steps.rb
@@ -17,6 +17,15 @@ When /^I enter "(.+)" in the (.+) with (.+) "(.+)"$/ do |value, element, locator
   @browser.send(element.to_sym, locator.to_sym => locator_value).set(value)
 end
 
+When /^I enter optional "(.+)" in the (.+) with (.+) "(.+)"$/ do |value, element, locator, locator_value|
+  begin
+    @browser.send(element.to_sym, locator.to_sym => locator_value).wd.location_once_scrolled_into_view 
+    @browser.send(element.to_sym, locator.to_sym => locator_value).set(value)
+  rescue Watir::Exception::UnknownObjectException
+    plog("The optional #{element} element with #{locator} \"#{locator_value}\" was not found. Resuming the test...","grey")
+  end
+end
+
 When /^I select "(.+)" on the (.+) with (.+) "(.+)"$/ do |value, element, locator, locator_value|
   @browser.send(element.to_sym, locator.to_sym => locator_value).wd.location_once_scrolled_into_view 
   @browser.send(element.to_sym, locator.to_sym => locator_value).select(value)
