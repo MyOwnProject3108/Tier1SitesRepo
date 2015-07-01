07f40ff5b061f2dcca5f247d7e036980e7f7e455
Zach Sulemain <zach.suleiman@peerius.com>
2015-07-01 16:32:48 +0100

    Navigation steps changes

--- features/step_definitions/order_steps.rb
+++ features/step_definitions/order_steps.rb
@@ -1,5 +1,6 @@
 
 When /^I add the current product to the basket$/ do
+@browser.execute_script("window.confirm = function() {return false}")
   @current_page.add_to_basket_element.when_present.click
 end
 
@@ -8,9 +9,11 @@ When /^I click checkout$/ do
 end
 
 When /^I remove all of the products from the basket$/ do
+  
 # links = @current_page.remove_product_links.collect{|x| x.attribute('href')}
  links = @current_page.remove_product_links
  links.each {|link|
 	link.click
  }
+ 
 end
